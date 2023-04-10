ca037bbe2f06f36fa48258730b474f1af8affe45
Haitsama <haitsam@students.amikom.ac.id>
2023-04-10 19:22:12 +0700

    void-overload

---
+++ void_overload/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace void_overload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            number numbers =new number();
+            numbers.CariNilaiMin(25, 24);
+            numbers.CariNilaiMin(25, 21,56);
+            numbers.CariNilaiMax(25, 24);
+            numbers.CariNilaiMax(25, 21, 26);
+        }
+    }
+}
